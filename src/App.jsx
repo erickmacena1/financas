43f116ab85a167ed75101e26f75534a4029537a4
Erick Araujo <erickmacena2@gmail.com>
2025-04-26 13:35:38 -0300

    add: html inicial

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import './App.css'
 function App() {
 
   return (<div>
-
+    <h1>Divisão de gastos</h1>
+    <hr />
+    <h2>Valor inicial</h2>
+    <input type="number" name="" id="" />
+    <h3>Investimento</h3>
+    <input type="number" name="" id="" />
+    <h3>Lazer</h3>
+    <input type="number" name="" id="" />
+    <h3>Sobra</h3>
+    <input type="number" name="" id="" />
   </div>)
 }
 
