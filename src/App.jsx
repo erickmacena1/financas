4762d57bdae87d07c771309f855584ae46ffa6cc
Erick Araujo <erickmacena2@gmail.com>
2025-06-05 07:57:56 -0300

    add: Estados iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
 import InputNumber from "./InputNumber"
 
 function App() {
+
+  const [initialValue, setInitialValue] = useState(undefined)
+  const [investmentPercent, setInvestmentPercent] = useState(undefined)
+  const [leisurePercent, setLeisurePercent] = useState(undefined)
+  
+  const [investmentValue, setInvestmentValue] = useState(undefined)
+  const [leisureValue, setLeisureValue] = useState(undefined)
+  const [remainingValue, setRemainingValue] = useState(undefined)
+  const [remainingPercent, setRemainingPercent] = useState(undefined)
 
   return (<div
     className='font-["Inter"] flex items-center flex-col px-4 space-y-4'
@@ -11,23 +21,18 @@ function App() {
 
     <div className="w-full space-y-3 flex items-center flex-col">
       <h2 className='text-lg font-medium'>Valor inicial (R$)</h2>
-      <InputNumber defaultValue={0} />
+      <InputNumber />
     </div>
 
     <div className="w-full space-y-3 flex items-center flex-col">
       <h2 className='text-lg font-medium'>Porcentagem de Investimento (%)</h2>
-      <InputNumber defaultValue={0} />
+      <InputNumber />
     </div>
 
     <div className="w-full space-y-3 flex items-center flex-col">
       <h2 className='text-lg font-medium'>Porcentagem de Lazer (%)</h2>
-      <InputNumber defaultValue={0} />
+      <InputNumber />
     </div>
-
-    {/* <div className="w-full space-y-3 flex items-center flex-col">
-      <h2 className='text-lg font-medium'>Restante</h2>
-      <InputNumber defaultValue={0} />
-    </div> */}
 
     <div className="w-full bg-slate-800 flex items-center flex-col p-4 space-y-4">
       {/* RESULTADOS */}
@@ -37,17 +42,17 @@ function App() {
 
       <div className="w-full space-y-3 flex items-center flex-col">
         <h2 className='text-lg font-medium'>Valor de Investimento (R$)</h2>
-        <InputNumber defaultValue={0} />
+        <InputNumber readOnly />
       </div>
 
       <div className="w-full space-y-3 flex items-center flex-col">
         <h2 className='text-lg font-medium'> Valor de Lazer (R$)</h2>
-        <InputNumber defaultValue={0} />
+        <InputNumber />
       </div>
 
       <div className="w-full space-y-3 flex items-center flex-col">
         <h2 className='text-lg font-medium'> Saldo (R$)</h2>
-        <InputNumber defaultValue={0} />
+        <InputNumber />
       </div>
     </div>
   </div>)
